3c02d7aada00980f1690709aa2dd3b7c21f3f5b0
光光 <topcheer@users.noreply.github.com>
2026-06-01 12:12:11 +0800

    Update ggcode-desktop Formula to v1.3.56

--- Formula/ggcode-desktop.rb
+++ Formula/ggcode-desktop.rb
@@ -8,11 +8,11 @@ class GgcodeDesktop < Formula
   on_linux do
     on_intel do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_linux_amd64"
-      sha256 "e69b939e2447b7f990e606367f85ea3b8b96afb765ebcecc9ea370384106f7e8"
+      sha256 "2bed8bce0a79046e652719bf72a3aa16c64c7e979f950fe55bf9eb7db3896848"
     end
     on_arm do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_linux_arm64"
-      sha256 "ab746d13ea8a7a70bf7ac853f3f85cf82a098bea490f92b311bf3971eea24f99"
+      sha256 "97eb1cd68978f590aad82391803fd3f5da6da4f7e48126d722e2338622657146"
     end
   end
 
