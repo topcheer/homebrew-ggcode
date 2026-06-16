1457a42495aa889f7dd0e3dd8c0e788a16d2e53c
光光 <topcheer@users.noreply.github.com>
2026-06-17 01:01:39 +0800

    Update ggcode-desktop Formula to v1.3.71

--- Formula/ggcode-desktop.rb
+++ Formula/ggcode-desktop.rb
@@ -8,22 +8,22 @@ class GgcodeDesktop < Formula
   on_macos do
     on_intel do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_darwin_amd64"
-      sha256 "607854f2d4f6376aab5ea0eb5d81af9135a2e6a264b4f4a2ddac3b278e3f85e9"
+      sha256 "8864631dab81fb0a664319e146e1abec42fdadf6d9d3bab00c7dd2f1cdd06640"
     end
     on_arm do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_darwin_arm64"
-      sha256 "e381fd05579102d37c05d6de2534634958feddf59af771a1e1c41a3b7a5e7a8b"
+      sha256 "5201a584c1bd0be705cea3adb1b378f3c77e1cbd65492fe80e4e86fd4bcff166"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_linux_amd64"
-      sha256 "51c33527263adf68190af829119df41f322749e2eba1cf49fc68a97eab7e681c"
+      sha256 "d7de08e3f1bf470d111f68b92e1ed1c89fb5161e242ca011d0f5833c790efd92"
     end
     on_arm do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_linux_arm64"
-      sha256 "6b0696252a6d7d9454b8fbbe9544a5f2ac1867cc9a4145397dfac7a5152696c3"
+      sha256 "a2d78df1fbc19360746af859d215b9f4b4217b7387c73b2a23c4246a855ae02e"
     end
   end
 
