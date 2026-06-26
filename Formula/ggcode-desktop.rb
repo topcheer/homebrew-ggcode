be4489913a3678c93f85afcf81573e64e28a6df6
光光 <topcheer@users.noreply.github.com>
2026-06-26 21:04:14 +0800

    Update ggcode-desktop Formula to v1.3.85

--- Formula/ggcode-desktop.rb
+++ Formula/ggcode-desktop.rb
@@ -2,28 +2,28 @@
 class GgcodeDesktop < Formula
   desc "AI coding agent with a native desktop interface."
   homepage "https://github.com/topcheer/ggcode"
-  version "1.3.84"
+  version "1.3.85"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_darwin_amd64"
-      sha256 "ebdf39ee7d9d351aea71e97430553fcb2fc56cc4255960f93ff27012f11982c8"
+      sha256 "61b7675041c1607c80038a7c83fe4d7a37fd51c15ef85830497a56463be292c9"
     end
     on_arm do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_darwin_arm64"
-      sha256 "d612710d408e6ec8632e91adff1d956fd469583888b5df177b97e45e089ac8a8"
+      sha256 "f8439d5a6998c019568969078d0ff7132ce5eeb229f0cc509fd0d954da04b2a6"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_linux_amd64"
-      sha256 "e764e8170e3f77ec14196b4f8e9b4d54749c383423e031c2bda206098dba33f2"
+      sha256 "71ef149a75ee584c59ea0e25c9b808db7731afb6fe9ba5747a6c4baf8d96b54a"
     end
     on_arm do
       url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_linux_arm64"
-      sha256 "d3b2ebad124a05e1f40759c19809612ca3312f5b8b7a4b3eaa655121d084fe84"
+      sha256 "53258ac498d78c9fe9f954ff0b094fc8a70f688cc29621b25c27879e92b44e6b"
     end
   end
 
