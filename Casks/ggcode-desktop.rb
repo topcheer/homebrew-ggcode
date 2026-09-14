b65bc042b17bb1422e9da1e96b95f2a8eae2f35b
光光 <topcheer@users.noreply.github.com>
2026-09-15 00:58:57 +0800

    Update ggcode-desktop Cask to v1.3.238

--- Casks/ggcode-desktop.rb
+++ Casks/ggcode-desktop.rb
@@ -1,9 +1,9 @@
 # This file is auto-generated. DO NOT EDIT.
 cask "ggcode-desktop" do
-  version "1.3.237"
+  version "1.3.238"
 
   url "https://github.com/topcheer/ggcode/releases/download/v#{version}/ggcode-desktop_#{version}_darwin_universal.dmg"
-  sha256 "ecb85ad01478c49e475cab34f07f55bf4635ce6002854d1a46ed20b4275962c5"
+  sha256 "a68745ca97b7a9ca8841dc40e4bdd5536e5bfa21160597242606a0b69b346949"
 
   name "GGCode Desktop"
   desc "AI coding agent with a native desktop interface."
